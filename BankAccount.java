bce04e58cdc68cbcc54dc43b543733d785a2bfda
pokiebob <ccursetjee@gmail.com>
2020-10-28 01:02:22 -0400

    newAccount function

--- BankAccount.java
+++ BankAccount.java
@@ -4,8 +4,15 @@ public class BankAccount {
   int accountID;
   String password;
 
+  void newAccount(int newID, String newPassword) {
+    accountID = newID;
+    password = newPassword;
+    balance = 0;
+  }
+
   public static void main(String[] args) {
     BankAccount myAccount = new BankAccount();
+    myAccount.newAccount(221153448, "pswd");
     System.out.println(myAccount.balance);
   }
 }
