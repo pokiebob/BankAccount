6d5fd53b5bd4ddb1380f7b46c23bea8533a902f8
pokiebob <ccursetjee@gmail.com>
2020-10-28 01:52:37 -0400

    transferred test cases to Tester.java

--- BankAccount.java
+++ BankAccount.java
@@ -43,18 +43,5 @@ public class BankAccount {
   }
 
   public static void main(String[] args) {
-    BankAccount myAccount = new BankAccount(1208, "pswd");
-    System.out.println(myAccount.getBalance());
-    System.out.println(myAccount.getAccountID());
-    myAccount.setPassword("new pswd");
-    System.out.println(myAccount.password);
-    System.out.println(myAccount.deposit(1000));
-    System.out.println(myAccount.deposit(-1000));
-    System.out.println(myAccount.getBalance());
-    System.out.println(myAccount.withdraw(-999));
-    System.out.println(myAccount.withdraw(1001));
-    System.out.println(myAccount.withdraw(999));
-    System.out.println(myAccount.getBalance());
-    System.out.println(myAccount.toString());
   }
 }
