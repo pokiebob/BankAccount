8d95aaf0cad74b4de645d3846ad91d8a3b05a6a1
pokiebob <ccursetjee@gmail.com>
2020-10-28 00:53:53 -0400

    private variables

---
+++ BankAccount.java
@@ -0,0 +1,11 @@
+public class BankAccount {
+
+  double balance;
+  int accountID;
+  String password;
+
+  public static void main(String[] args) {
+    BankAccount myAccount = new BankAccount();
+    System.out.println(myAccount.balance);
+  }
+}
